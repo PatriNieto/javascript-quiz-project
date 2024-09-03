9e2baecf86eb2bf96d716ff7fcaeb7d3545a93e3
Javier Gascon <javitocatral@gmail.com>
2024-09-03 17:01:06 +0200

    segundo dia quiz

--- src/quiz.js
+++ src/quiz.js
@@ -1,60 +1,73 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor(questions, timeLimit, timeRemaining){
-       
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    } 
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions
+    this.timeLimit = timeLimit
+    this.timeRemaining = timeRemaining
+    this.correctAnswers = 0
+    this.currentQuestionIndex = 0
+  }
 
-    // 2. getQuestion()
+  // 2. getQuestion()
 
-    getQuestion() {
-        return this.questions[this.currentQuestionIndex];
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex]
+  }
+
+  // 3.
+  moveToNextQuestion() {
+    this.currentQuestionIndex++
+  }
+
+  // 4.
+  shuffleQuestions() {
+    // Definir el array global
+
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+
+      // Intercambiar los elementos en las posiciones i y j
+      const temp = this.questions[i]
+      this.questions[i] = this.questions[j]
+      this.questions[j] = temp
     }
+    console.log(this.questions)
+    return this.questions
+  }
 
-    // 3. 
-    moveToNextQuestion() {
-        this.currentQuestionIndex++;
+  // 5.
+  checkAnswer(answer) {
+    if (answer) {
+      this.correctAnswers++
     }
+  }
 
-    // 4. 
-    shuffleQuestions() {
-        // Definir el array global
-
-        for (let i = this.questions.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        
-        // Intercambiar los elementos en las posiciones i y j
-        const temp = this.questions[i];
-        this.questions[i] = this.questions[j];
-        this.questions[j] = temp;
+  // 6.
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true
     }
-        console.log(this.questions);
-         return this.questions;
-    }   
-    
-
-
-    
-
-    // 5. 
-    checkAnswer(answer){
-        if(answer) {
-            this.correctAnswers++;
-        }
+  }
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter((cadaCuestion) => {
+        return difficulty === cadaCuestion.difficulty
+      })
     }
-
-    // 6. 
-    hasEnded(){
-        if(this.currentQuestionIndex < this.questions.length){
-            return false;
-        } else if (this.currentQuestionIndex === this.questions.length){
-            return true;
-        }
-    }
+  }
+  averageDifficulty() {
+    let sum = this.questions.reduce((acc, eachQuestions) => {
+      if (this.questions.length === 0 && !eachQuestions.difficulty) {
+        return acc
+      } else {
+        return acc + eachQuestions.difficulty
+      }
+    }, 0)
+    let media = sum / this.questions.length
+    return media
+  }
 }
