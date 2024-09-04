beaf2bf8e1fc0d2334f80d6bb6017fecba57c7e8
PatriNieto <patricianieto2.0@gmail.com>
2024-09-04 17:48:02 +0200

    casi

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const questionCount = document.querySelector("#questionCount");
   const questionContainer = document.querySelector("#question");
   const choiceContainer = document.querySelector("#choices");
+  
   const nextButton = document.querySelector("#nextButton");
 
   // End view elements
@@ -39,6 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   // Create a new Quiz instance object
   const quiz = new Quiz(questions, quizDuration, quizDuration);
+  console.log(quiz);
   // Shuffle the quiz questions
   quiz.shuffleQuestions();
 
@@ -51,6 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Display the time remaining in the time remaining container
   const timeRemainingContainer = document.getElementById("timeRemaining");
+  
   timeRemainingContainer.innerText = `${minutes}:${seconds}`;
 
   // Show first question
@@ -76,7 +79,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+
+
   function showQuestion() {
+
+    //preguntar por que las variables tienen que ir aqui 
+
+
     // If the quiz has ended, show the results
     if (quiz.hasEnded()) {
       showResults();
@@ -84,33 +93,48 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // Clear the previous question text and question choices
-    questionContainer.innerText = "";
-    choiceContainer.innerHTML = "";
+   /*  questionContainer.innerText = "";
+    choiceContainer.innerHTML = ""; */
 
     // Get the current question from the quiz by calling the Quiz class method `getQuestion()`
     const question = quiz.getQuestion();
+    console.log(question);
     // Shuffle the choices of the current question by calling the method 'shuffleChoices()' on the question object
     question.shuffleChoices();
     
-    
-
     // YOUR CODE HERE:
     //
     // 1. Show the question
     // Update the inner text of the question container element and show the question text
-
     
     // 2. Update the green progress bar
     // Update the green progress bar (div#progressBar) width so that it shows the percentage of questions answered
     
-    progressBar.style.width = `65%`; // This value is hardcoded as a placeholder
+    
+    let progressWidth = 10
+    const questionNode = document.querySelector("#question")
+    let quizShuffledQuestArr = quiz.shuffleQuestions()
+    console.log( quizShuffledQuestArr);
+    
+   /*  let unicaPregunta = quizShuffledQuestArr.map((eachQuest,i) => {
+      return eachQuest.text
+      
+    }); */
+
+
+    questionNode.innerHTML += `<p>  ${question.text }</p>` 
+
+    progressWidth = quiz.currentQuestionIndex + 10
+    
+    progressBar.style.width = `${progressWidth}%`; // This value is hardcoded as a placeholder
+    
 
 
 
     // 3. Update the question count text 
     // Update the question count (div#questionCount) show the current question out of total questions
     
-    questionCount.innerText = `Question 1 of 10`; //  This value is hardcoded as a placeholder
+    questionCount.innerText = `Question ${quiz.currentQuestionIndex + 1} of 10`; //  This value is hardcoded as a placeholder
 
 
     
@@ -118,11 +142,28 @@ document.addEventListener("DOMContentLoaded", () => {
     // Loop through the current question `choices`.
       // For each choice create a new radio input with a label, and append it to the choice container.
       // Each choice should be displayed as a radio input element with a label:
-      /* 
-          <input type="radio" name="choice" value="CHOICE TEXT HERE">
-          <label>CHOICE TEXT HERE</label>
-        <br>
-      */
+      let respuestasArr = quizShuffledQuestArr.map((eachQuest,i) => {
+        console.log(i);
+        return eachQuest.choices
+      });
+
+      
+
+      //questionNode.innerHTML += `<p> Hola ${unicaPregunta[0] }</p>` 
+      let choices = question.choices
+      
+      console.log("p",choices);
+      choices.forEach((element, i) => {
+        console.log(element);
+        return choiceContainer.innerHTML +=`<input type="radio" name="choice" value="CHOICE TEXT HERE">
+          <label>${element}</label>
+        <br>`
+         
+      });
+      
+       
+         
+      
       // Hint 1: You can use the `document.createElement()` method to create a new element.
       // Hint 2: You can use the `element.type`, `element.name`, and `element.value` properties to set the type, name, and value of an element.
       // Hint 3: You can use the `element.appendChild()` method to append an element to the choices container.
@@ -134,13 +175,24 @@ document.addEventListener("DOMContentLoaded", () => {
   
   function nextButtonHandler () {
     let selectedAnswer; // A variable to store the selected answer value
+    //choiceContainer es el 
+    /* objeto quiz
+  checkAnswer(answer) {
+    if (answer) {
+      this.correctAnswers++
+    }
+  } */ 
+    console.log(choiceContainer.value);
+    respuesta.value.forEach((element) => {
+      checkAnswer(element)
+    });
 
 
 
     // YOUR CODE HERE:
     //
     // 1. Get all the choice elements. You can use the `document.querySelectorAll()` method.
-
+    
 
     // 2. Loop through all the choice elements and check which one is selected
       // Hint: Radio input elements have a property `.checked` (e.g., `element.checked`).
